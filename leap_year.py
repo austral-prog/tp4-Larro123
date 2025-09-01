25ea5f89ef50c2632b7bf7348cc29dad57536c58
Larro123 <slarrosa@mail.austral.edu.ar>
2025-09-01 12:41:09 -0300

    Add files via upload

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,11 @@
 def leap_year():
-    print("TO DO")
+    
+    año = input("Ingresar un año:")
+    año_fl = float(año)
+    año_div = año_fl % 4
+    año_cent = año_fl %400
+    if año_div == 0 and año_cent == 0:
+        print(f"El año {año} es bisiesto")
+    else:
+        print(f"El año {año} no es bisiesto")
+        
